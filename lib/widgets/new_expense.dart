ac0958e83cfdcdb89d5b2eb640557758cd270c6a
Hamid Ali <hamidalig942@gmail.com>
2026-04-07 16:26:43 +0500

    add: button to save new expense with updated title

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,15 +8,34 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  String enteredTitle = '';
+
+  void _saveTitle(String inputValue) {
+    enteredTitle = inputValue;
+  }
+
+  void _onPressSaveExpense() {
+    print(enteredTitle);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            onChanged: _saveTitle,
             maxLength: 50,
             decoration: InputDecoration(label: Text('Title')),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: _onPressSaveExpense,
+                child: Text('Save Expense'),
+              ),
+            ],
           ),
         ],
       ),
