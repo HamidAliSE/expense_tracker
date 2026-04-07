c3d7501d92d0e2b7d1dc525e60dc122f51bcb088
Hamid Ali <hamidalig942@gmail.com>
2026-04-07 15:16:32 +0500

    add: expense details card in expense_item

---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+        child: Column(
+          children: [
+            Text(expense.title),
+            Row(
+              children: [
+                Text(expense.amount.toStringAsFixed(2)),
+                Spacer(),
+                Row(
+                  children: [Icon(Icons.alarm), Text(expense.date.toString())],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
